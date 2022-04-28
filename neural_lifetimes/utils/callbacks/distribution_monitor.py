603774577cea67670959b80f03edc6b30b47b6ac
Cornelius Emde <cornelius.emde@wise.com>
2022-04-28 16:22:48 +0100

    Fix formatting

--- neural_lifetimes/utils/callbacks/distribution_monitor.py
+++ neural_lifetimes/utils/callbacks/distribution_monitor.py
@@ -2,7 +2,6 @@ import collections
 import numpy as np
 import pytorch_lightning as pl
 import torch
-import torch.nn as nn
 import torch.distributions as d
 
 from torchmetrics import MetricCollection
