9f1b2788bcd7a03379632fe1e3ef65ea27c9f14f
Cornelius Emde <cornelius.emde@wise.com>
2022-05-06 18:36:14 +0100

    added tokenizer tests and docstring

--- neural_lifetimes/utils/data/tokenizer.py
+++ neural_lifetimes/utils/data/tokenizer.py
@@ -1,4 +1,4 @@
-from typing import List, Any, Dict
+from typing import List, Dict
 
 from dataclasses import dataclass
 
@@ -7,6 +7,32 @@ import numpy as np
 
 @dataclass
 class Tokenizer:
+    """A callable class to tokenize your dictionary-based batches.
+
+    The Tokenizer left-truncates sequences and appends start tokens.
+
+    Args:
+        continuous_features (List[str]): A list containing the names of the continuous features.
+        discrete_features (List[str]): A list containing the names of the discrete features.
+        max_item_len (int): The maximum length to which the sequence should be truncated.
+            The tokenizer performs left-truncation. The length of returned sequences will be ``max_item_len + 1``.
+        start_token_continuous (np.float32): The start token for variables specified in ``continuous_features``.
+        start_token_discrete (str): The start token for variables specified in ``discrete_features``.
+        start_token_other (np.float32): The start token for variables not specified in ``continuous_features``
+            or `discrete_features``.
+
+    Attributes:
+        continuous_features (List[str]): A list containing the names of the continuous features.
+        discrete_features (List[str]): A list containing the names of the discrete features.
+        features (List[str]): A list containing the names of both continuous and discrete features.
+        max_item_len (int): The maximum length to which the sequence should be truncated.
+            The tokenizer performs left-truncation. The length of returned sequences will be ``max_item_len + 1``.
+        start_token_continuous (np.float32): The start token for variables specified in ``continuous_features``.
+        start_token_discrete (str): The start token for variables specified in ``discrete_features``.
+        start_token_other (np.float32): The start token for variables not specified in ``continuous_features``
+            or `discrete_features``.
+    """
+
     continuous_features: List[str]
     discrete_features: List[str]
     max_item_len: int
@@ -15,14 +41,21 @@ class Tokenizer:
     start_token_other: np.float32
 
     def __call__(self, x: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
+        """Tokenizes and truncates the data ``x``.
 
+        Args:
+            x (Dict[str, np.ndarray]): The raw data dictionary.
+
+        Returns:
+            Dict[str, np.ndarray]: The transformed data.
+        """
         # truncate sequence
         x = {k: v[-(self.max_item_len) :] for k, v in x.items()}
 
         # add start tokens
         for k, v in x.items():
             if k in self.features:
-                if k in self.discrete_features.keys():
+                if k in self.discrete_features:
                     x[k] = np.append([self.start_token_discrete], v)
                 else:
                     x[k] = np.append([self.start_token_continuous], v)
@@ -31,5 +64,5 @@ class Tokenizer:
         return x
 
     @property
-    def features(self):
-        return self.continuous_features + list(self.discrete_features.keys())
+    def features(self) -> List[str]:
+        return self.continuous_features + list(self.discrete_features)
---
+++ tests/test_utils/test_data/test_tokenizer.py
@@ -0,0 +1,53 @@
+from typing import Dict
+import numpy as np
+
+import pytest
+
+from neural_lifetimes.utils.data import Tokenizer
+
+
+@pytest.fixture
+def data():
+    return {
+        "CF1": np.array([1, 2, 3, 4]),
+        "CF2": np.array([4, 3, 2, 1]),
+        "Other_CF": np.array([0, 1, 0, 1]),
+        "DF1": np.array(["level_1", "level_1", "level_2", "level_3"]),
+        "Other_DF": np.array(["level_1", "level_1", "level_2", "level_3"]),
+    }
+
+
+@pytest.fixture
+def tokenizer():
+    return Tokenizer(
+        continuous_features=["CF1", "CF2"],
+        discrete_features=["DF1"],
+        max_item_len=2,
+        start_token_continuous=float("inf"),
+        start_token_discrete="BigBang",
+        start_token_other=-1,
+    )
+
+
+def test_constructor(tokenizer):
+    assert tokenizer
+
+
+def test_call(data: Dict[str, np.ndarray], tokenizer: Tokenizer):
+    tokenized = tokenizer(data)
+
+    expected = {
+        "CF1": np.array([np.inf, 3, 4]),
+        "CF2": np.array([np.inf, 2, 1]),
+        "Other_CF": np.array([-1, 0, 1]),
+        "DF1": np.array(["BigBang", "level_2", "level_3"]),
+        "Other_DF": np.array([-1, "level_2", "level_3"]),
+    }
+
+    for k in tokenized.keys():
+        assert np.all(tokenized[k] == expected[k])
+
+
+def test_features(tokenizer: Tokenizer):
+    feat = tokenizer.features
+    assert feat == ["CF1", "CF2", "DF1"]
