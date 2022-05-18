909a8b62aff6bb7cd12886096ccf556e4d5c4848
Cornelius Emde <cornelius.emde@wise.com>
2022-05-18 19:16:38 +0100

    added date conversions to utils implemented tests

--- neural_lifetimes/utils/date_arithmetic.py
+++ neural_lifetimes/utils/date_arithmetic.py
@@ -1,4 +1,7 @@
 import datetime
+from typing import Union
+import numpy as np
+import torch
 
 
 def date2time(date: datetime.date) -> datetime.datetime:
@@ -12,3 +15,36 @@ def date2time(date: datetime.date) -> datetime.datetime:
         datetime.datetime: The converted date.
     """
     return datetime.datetime.combine(date, datetime.datetime.min.time())
+
+
+DatetimeLike = Union[torch.tensor, np.array, datetime.datetime]
+TimestampLike = Union[torch.tensor, np.array, float, int]
+
+
+def datetime2float(t: DatetimeLike, unit: str = "h") -> TimestampLike:
+    if isinstance(t, datetime.datetime):
+        t = t.timestamp() * 1e6
+    elif isinstance(t, np.ndarray):
+        t = t.astype("datetime64[us]").astype(np.int64).astype(np.float32)
+    elif isinstance(t, torch.tensor):
+        raise NotImplementedError
+    else:
+        raise TypeError(f"Datetime must be of type 'DatetimeLike'. Got '{type(t).__name__}'")
+    # time is in microseconds
+    t = t / (1e6 * 60 * 60)
+    # time is in hours
+    return t
+
+
+def float2datetime(t: TimestampLike, unit: str = "h"):
+    t = t * (1e6 * 60 * 60)
+    if isinstance(t, (float, int)):
+        t = datetime.datetime.fromtimestamp(t / 1e6)
+    elif isinstance(t, np.ndarray):
+        t = t.astype("datetime64[us]")
+    elif isinstance(t, torch.tensor):
+        raise NotImplementedError
+    else:
+        raise TypeError(f"Datetime must be of type 'DatetimeLike'. Got '{type(t).__name__}'")
+
+    return t
---
+++ tests/test_utils/test_date_arithmetic.py
@@ -0,0 +1,86 @@
+from datetime import datetime, timedelta
+
+import numpy as np
+import torch
+
+import pytest
+from neural_lifetimes.utils.date_arithmetic import datetime2float, float2datetime
+
+
+@pytest.fixture
+def datetimes():
+    return [datetime(2000 + i, i, 2 * i) for i in range(1, 13)]
+
+
+@pytest.fixture
+def timestamps():
+    return [
+        271776.0,
+        281328.0,
+        290808.0,
+        300383.0,
+        309911.0,
+        319463.0,
+        328991.0,
+        338567.0,
+        348119.0,
+        357647.0,
+        367200.0,
+        376752.0,
+    ]
+
+
+# tolerance in hours
+@pytest.fixture
+def error_tolerance():
+    return 2
+
+
+class Test_Base:
+    @staticmethod
+    def test_datetime2float(datetimes, timestamps, error_tolerance):
+        res = [datetime2float(dt) for dt in datetimes]
+        assert all([(r - t) < error_tolerance for r, t in zip(res, timestamps)])
+
+    @staticmethod
+    def test_float2datetime(datetimes, timestamps, error_tolerance):
+        res = [float2datetime(ts) for ts in timestamps]
+        assert all([(r - t) < timedelta(hours=error_tolerance) for r, t in zip(res, datetimes)])
+
+    @staticmethod
+    def test_both(datetimes, error_tolerance):
+        res = [float2datetime(datetime2float(dt)) for dt in datetimes]
+        assert all([(r - t) < timedelta(hours=error_tolerance) for r, t in zip(res, datetimes)])
+
+
+class Test_Numpy:
+    @staticmethod
+    def test_datetime2float(datetimes, timestamps, error_tolerance):
+        datetimes = np.array(datetimes, dtype="datetime64[us]")
+        timestamps = np.array(timestamps)
+        res = datetime2float(datetimes)
+        assert np.all(np.isclose(res, timestamps, rtol=0, atol=error_tolerance))
+
+    @staticmethod
+    def test_float2datetime(datetimes, timestamps, error_tolerance):
+        datetimes = np.array(datetimes, dtype="datetime64[us]")
+        timestamps = np.array(timestamps)
+        res = float2datetime(timestamps)
+        assert np.all((res - datetimes) < timedelta(hours=error_tolerance))
+
+    @staticmethod
+    def test_both(datetimes, timestamps, error_tolerance):
+        datetimes = np.array(datetimes, dtype="datetime64[us]")
+        timestamps = np.array(timestamps)
+        res = float2datetime(datetime2float(datetimes))
+        assert np.all((res - datetimes) < timedelta(hours=error_tolerance))
+
+
+class Test_AcrossTypes:
+    @staticmethod
+    def test_numpy_base():
+        assert False
+
+    @staticmethod
+    def test_numpy_torch():
+        assert False
