af3dc61692faa77e4b5f05b515267345c45a7af4
pereplanellTW <pere.planell@transferwise.com>
2022-06-24 14:19:11 +0100

    update _ranges functions

--- neural_lifetimes/utils/postgres/postgres_ingest.py
+++ neural_lifetimes/utils/postgres/postgres_ingest.py
@@ -6,13 +6,8 @@ import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 
-def postgres_ranges(
-    engine: Engine,
-    discr_feat: Sequence[str],
-    table_name: str
-) -> Dict[str, np.ndarray]:
-
-    out = {f: np.array(engine.execute(f"SELECT DISTINCT {f} from {table_name}").fetchall()) for f in discr_feat}
+def postgres_ranges(engine: Engine, discr_feat: Sequence[str], table_name: str) -> Dict[str, np.ndarray]:
+    out = {f: np.array(engine.execute(f"""SELECT DISTINCT "{f}" from {table_name}""").fetchall()) for f in discr_feat}
     return out
 
 def dtypes_from_table(
